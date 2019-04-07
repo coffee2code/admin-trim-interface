14a47425f6a11a77ee5e70e3f1016ee17886d0df
Scott Reilly <scott@coffee2code.com>
2019-04-06 23:34:19 -0600

    Update copyright date (2019)

--- admin-trim-interface.php
+++ admin-trim-interface.php
@@ -29,7 +29,7 @@
  */
 
 /*
-	Copyright (c) 2009-2018 by Scott Reilly (aka coffee2code)
+	Copyright (c) 2009-2019 by Scott Reilly (aka coffee2code)
 
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
